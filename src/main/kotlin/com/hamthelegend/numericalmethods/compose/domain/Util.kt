fdd1f090c1096fc0ec42610efbd690bbd39af1ad
hamthelegend <hamuelxd@gmail.com>
2022-03-12 02:05:54 +0800

    [WIP] Progress on MainScreen.kt

---
+++ src/main/kotlin/com/hamthelegend/numericalmethods/compose/domain/Util.kt
@@ -0,0 +1,83 @@
+package com.hamthelegend.numericalmethods.compose.domain
+
+import methods.common.Fx
+import methods.common.IterationResult
+import methods.iterativebracketing.BracketInterval
+import methods.iterativebracketing.runBisection
+import methods.iterativebracketing.runFalsePosition
+import methods.open.runFixedPoint
+import methods.open.runNewtonRaphson
+import methods.open.runSecant
+import java.math.BigDecimal
+import java.math.RoundingMode
+
+fun solveRoot(
+    method: Method,
+    f: Fx,
+    xL: BigDecimal,
+    xR: BigDecimal,
+    g: Fx,
+    fPrime: Fx,
+    initialX: BigDecimal,
+    initialXA: BigDecimal,
+    initialXB: BigDecimal,
+    minIterations: Int,
+    maxIterations: Int,
+    scale: Int,
+    roundingMode: RoundingMode,
+) = when (method) {
+    Method.BISECTION -> runBisection(
+        f = f,
+        initialInterval = BracketInterval(xL, xR),
+        minIterations = minIterations,
+        maxIterations = maxIterations,
+        scale = scale,
+        roundingMode = roundingMode,
+    )
+    Method.FALSE_POSITION -> runFalsePosition(
+        f = f,
+        initialInterval = BracketInterval(xL, xR),
+        minIterations = minIterations,
+        maxIterations = maxIterations,
+        scale = scale,
+        roundingMode = roundingMode,
+    )
+    Method.FIXED_POINT -> runFixedPoint(
+        g = g,
+        initialX = initialX,
+        minIterations = minIterations,
+        maxIterations = maxIterations,
+        scale = scale,
+        roundingMode = roundingMode
+    )
+    Method.NEWTON_RAPHSON -> runNewtonRaphson(
+        f = f,
+        fPrime = fPrime,
+        initialX = initialX,
+        minIterations = minIterations,
+        maxIterations = maxIterations,
+        scale = scale,
+        roundingMode = roundingMode
+    )
+    Method.SECANT -> runSecant(
+        f = f,
+        initialXA = initialXA,
+        initialXB = initialXB,
+        minIterations = minIterations,
+        maxIterations = maxIterations,
+        scale = scale,
+        roundingMode = roundingMode
+    )
+}
+
+val IterationResult.tableString: String
+    get() {
+        val stringBuilder = StringBuilder(columnNamesCsv.values.joinToString("\t"))
+        stringBuilder.append("\n")
+        for ((index, iteration) in iterations.withIndex()) {
+            stringBuilder.append("$index\t")
+            stringBuilder.append(iteration.valuesCsv.values.joinToString("\t"))
+            stringBuilder.append("\n")
+        }
+        return stringBuilder.toString()
+    }
