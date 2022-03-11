c73eb6d1d09dbac22e2a5fe7b3b0cfe738b813cb
hamthelegend <hamuelxd@gmail.com>
2022-03-12 00:45:05 +0800

    Added space as default wordSeparator for String.toTitleCase()

--- src/main/kotlin/com/hamthelegend/numericalmethods/compose/extensions/FormatString.kt
+++ src/main/kotlin/com/hamthelegend/numericalmethods/compose/extensions/FormatString.kt
@@ -1,6 +1,6 @@
 package com.hamthelegend.numericalmethods.compose.extensions
 
-fun String.toTitleCase(wordSeparator: String): String {
+fun String.toTitleCase(wordSeparator: String = " "): String {
     val stringBuilder = StringBuilder()
     val words = lowercase().split(wordSeparator)
     for (word in words) {
