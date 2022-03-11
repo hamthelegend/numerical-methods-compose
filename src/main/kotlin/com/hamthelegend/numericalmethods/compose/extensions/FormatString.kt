fdc22de914c5ff20739d43f71e3da7234e393f43
hamthelegend <hamuelxd@gmail.com>
2022-03-12 01:24:17 +0800

    Fixed String.toTitleCase()
    BUG: It is not adding wordSeparators

--- src/main/kotlin/com/hamthelegend/numericalmethods/compose/extensions/FormatString.kt
+++ src/main/kotlin/com/hamthelegend/numericalmethods/compose/extensions/FormatString.kt
@@ -1,10 +1,11 @@
 package com.hamthelegend.numericalmethods.compose.extensions
 
-fun String.toTitleCase(wordSeparator: String = " "): String {
+fun String.toTitleCase(wordSeparator: String = " ", newWordSeparator: String = " "): String {
     val stringBuilder = StringBuilder()
     val words = lowercase().split(wordSeparator)
-    for (word in words) {
+    for ((index, word) in words.withIndex()) {
         stringBuilder.append(word.replaceFirstChar { it.uppercase() })
+        if (index != words.lastIndex) stringBuilder.append(newWordSeparator)
     }
     return stringBuilder.toString()
 }
